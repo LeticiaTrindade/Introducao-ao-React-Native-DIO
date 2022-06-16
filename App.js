50e030eba80e23d3a933a9182a1361373d62e82a
Letícia <leticiatrindade0504@gmail.com>
2022-06-16 16:45:58 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, Image, View, SafeAreaView, StatusBar , Pressable, Linking} from 'react-native';
 
-export default function App() {
+const colorGithub = '#010409';
+const colorFontGithub = '#C9D1D9';
+const colorFontDarkGithub = '#4F565E';
+
+const linkProfile = 'https://github.com/LeticiaTrindade/'
+const profileImage = 'https://avatars.githubusercontent.com/u/61462194?v=4';
+
+
+
+const App = () => {
+
+  const handlePressGoGithub = async ()  => {
+    console.log("Verificando link");
+    const res = await Linking.canOpenURL(linkProfile);
+    console.log("Link aprovado");
+    if (res){
+      Linking.openURL(linkProfile);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}> 
+    <StatusBar backgroundColor={colorGithub} barStyle='light-content' />
+      <View style={styles.container}>
+        <Image 
+          accessibilityLabel='Apenas o rosto de letícia com o fundo branco.'
+          style={styles.imgProfile} 
+          source={{uri: profileImage}}/>
+
+        <Text 
+        accessibilityLabel='Nome: Letícia Trindade'
+        style={[styles.defaultText, styles.name]}>Letícia Trindade</Text>
+
+        <Text 
+        accessibilityLabel='Nickname: Leticia Trindade'
+        style={[styles.defaultText, styles.nickname]}>LeticiaTrindade</Text>
+
+        <Text 
+        accessibilityLabel='Descrição: Estudante de Licenciatura em Computação na Universidade Federal Rural de Pernambuco e professoa de programação na escola Byjus FutureSchool. @leticiatrindadet'
+        style={[styles.defaultText, styles.description]}>Estudante de Licenciatura em Computação na Universidade Federal Rural de Pernambuco e professoa de programação na escola Byju's FutureSchool. @leticiatrindadet
+        </Text>
+
+        <Pressable onPress={handlePressGoGithub}> 
+          <View style={styles.button}>
+            <Text style={[styles.defaultText, styles.colorFontDarkGithub]}>Opn in Github</Text>
+          </View>
+
+        <StatusBar style="auto" />
+        </Pressable>
+      </View>
+
+    </SafeAreaView>
+);
+
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, //expande para a tela inteira.
+    backgroundColor: colorGithub,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
+
+  imgProfile: {
+    width: 250,
+    height: 250,
+    borderRadius: 125,
+    borderWidth: 2,
+    borderColor: 'gray',
+  },
+
+  defaultText: {
+    color: colorFontGithub,
+    textAlign: 'justify', 
+     
+  },
+
+  name: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+
+  nickname: {
+    color: colorFontDarkGithub,
+    fontSize: 15,
+  },
+
+  description: {
+    fontSize: 17,
+    fontWeight: 'bold', 
+  },
+
+  button: {
+    backgroundColor: colorFontDarkGithub,
+    borderRadius: 40,
+    padding: 20,
+  }
+  
+
 });
